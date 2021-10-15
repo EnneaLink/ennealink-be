a7038c7b6f8f926c1f70fc0c4656f1968b8c0a1d
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-10-15 08:36:16 -0600

    Add bcrypt and password to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_224835) do
+ActiveRecord::Schema.define(version: 2021_10_15_143221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_10_13_224835) do
     t.bigint "enneagram_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["enneagram_id"], name: "index_users_on_enneagram_id"
     t.index ["myers_brigg_id"], name: "index_users_on_myers_brigg_id"
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,16 @@ RSpec.describe User, type: :model do
     it { should belong_to(:myers_brigg) }
     it { should belong_to(:enneagram) }
     it { should have_many(:friendships) }
+    it { should have_many(:friends).through(:friendships) }
   end
 
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
+    it { should allow_value('k@gmail.com').for(:email) }
+    it { should_not allow_value('kgmail.com').for(:email) }
     it { should validate_presence_of(:username) }
     it { should validate_uniqueness_of(:username) }
+    it { should validate_presence_of(:password) }
   end
 end
