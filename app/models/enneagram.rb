96bc190e83b84f9a2521397bede11e31e6f46b71
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-10-15 08:29:48 -0600

    Add number inclusion validation

--- app/models/enneagram.rb
+++ app/models/enneagram.rb
@@ -1,7 +1,7 @@
 class Enneagram < ApplicationRecord
   has_many :users
 
-  validates :number, presence: true
+  validates :number, presence: true, :inclusion => 1..9
   validates :name, presence: true
   validates :description, presence: true
 end
--- spec/models/enneagram_spec.rb
+++ spec/models/enneagram_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe Enneagram, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:number) }
+    it { should allow_value(1).for(:number) }
+    it { should allow_value(9).for(:number) }
+    it { should_not allow_value(0).for(:number) }
+    it { should_not allow_value(10).for(:number) }
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
   end
