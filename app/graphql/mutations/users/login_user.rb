5d2b939917496c0dd3f4c960c333f67918f25eba
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-10-23 16:39:05 -0600

    Create login function

---
+++ app/graphql/mutations/users/login_user.rb
@@ -0,0 +1,33 @@
+module Mutations
+  module Users
+    class LoginUser < GraphQL::Schema::Mutation
+      argument :username, String, required: true do
+        description "The user's username"
+      end
+
+      argument :password, String, required: true do
+        description "The user's password"
+      end
+
+      field :success, Boolean, null: false
+      field :id, ID, null: false
+
+      def resolve(attributes)
+        user = User.find_by(username: attributes[:username])
+        valid_sign_in = user.present? && user.authenticate(attributes[:password])
+        
+        if valid_sign_in
+          {
+            success: true,
+            id: valid_sign_in.id
+          }
+        else
+          {
+            success: false,
+            id: 'nil'
+          }
+        end
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,7 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::Users::CreateUser
-
+    field :login_user, mutation: Mutations::Users::LoginUser
     field :update_user, mutation: Mutations::Users::UpdateUser
   end
 end
