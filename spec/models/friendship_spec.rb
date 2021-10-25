40a912ca275bfc92db50af4318dc4dc931f07ff5
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-10-25 15:49:51 -0600

    Add unique validation for friendships

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Friendship, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:friend) }
   end
+
+  describe 'validations' do
+    it { should validate_uniqueness_of(:friend_id).scoped_to(:user_id) }
+  end
 end
