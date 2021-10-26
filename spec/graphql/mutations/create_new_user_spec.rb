b3a93522dfa33a8e60df49500c458dfc531a15e0
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-10-25 21:12:38 -0600

    Add sad paths to user update and create user

--- spec/graphql/mutations/create_new_user_spec.rb
+++ spec/graphql/mutations/create_new_user_spec.rb
@@ -26,8 +26,24 @@ module Mutations
           post '/graphql', params: { query: query }
 
           json = JSON.parse(response.body)
-          
+
           expect(json['errors'][0]['message']).to eq('Username has already been taken')
+        end
+
+        it 'returns error message if password is not given' do
+          post '/graphql', params: { query: query2 }
+
+          json = JSON.parse(response.body)
+
+          expect(json['errors'][0]['message']).to eq("Password can't be blank")
+        end
+
+        it 'returns error message if username is not given' do
+          post '/graphql', params: { query: query3 }
+
+          json = JSON.parse(response.body)
+
+          expect(json['errors'][0]['message']).to eq("Username can't be blank")
         end
       end
 
@@ -38,6 +54,42 @@ module Mutations
           authProvider: {
             credentials: {
               username: "Gertie",
+              password: "password"
+            }
+          }
+          ) {
+            id
+            username
+            }
+          }
+        GQL
+      end
+
+      def query2
+        <<~GQL
+        mutation {
+          user: createUser(
+          authProvider: {
+            credentials: {
+              username: "Gertie",
+              password: ""
+            }
+          }
+          ) {
+            id
+            username
+            }
+          }
+        GQL
+      end
+
+      def query3
+        <<~GQL
+        mutation {
+          user: createUser(
+          authProvider: {
+            credentials: {
+              username: "",
               password: "password"
             }
           }
